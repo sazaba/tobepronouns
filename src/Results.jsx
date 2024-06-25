3104faa5a7fe52cb71f82ed49ec4451d5796931c
Santiago Zappa <santiagozappab@gmail.com>
2024-06-25 15:08:42 -0500

    second

--- src/Results.jsx
+++ src/Results.jsx
@@ -2,11 +2,12 @@ import React, { useEffect } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import Passed from './assets/passed.webp';
 import Failed from './assets/failed.webp';
+import confetti from 'canvas-confetti';
 
 function Results() {
     const location = useLocation();
     const score = location.state.score;
-    const percentage = location.state.percentage;
+    const percentage = Math.round(location.state.percentage); // Redondear el porcentaje
 
     let resultTitle = "";
     let imageSrc = "";
@@ -15,6 +16,7 @@ function Results() {
     if (percentage >= 80) {
         resultTitle = "Congratulations, you passed this test!";
         imageSrc = Passed;
+        confetti();
     } else {
         resultTitle = "You did not pass this test.";
         imageSrc = Failed;
@@ -25,7 +27,7 @@ function Results() {
     }, []);
 
     return (
-        <div className={`relative ${bgColorClass} flex flex-col items-center text-center font-custom `}>
+        <div className={`relative ${bgColorClass} flex flex-col items-center text-center font-custom`}>
             <img className='w-40' src={imageSrc} alt='' />
 
             <div className="relative z-10 space-y-1">
@@ -33,7 +35,7 @@ function Results() {
                 {/* <p>Your Score: {score}</p> */}
                 <p>Percentage: {percentage}%</p>
                 <Link to="/">
-                    <button className=' bg-slangup hover:bg-white hover:text-slangup text-white font-bold mt-2 py-2 px-4 rounded mx-auto 
+                    <button className='bg-slangup hover:bg-white hover:text-slangup text-white font-bold mt-2 py-2 px-4 rounded mx-auto 
                 @screen md:w-[70%]
                 @screen sm:w-[70%]'>Retry Test</button>
                 </Link>
